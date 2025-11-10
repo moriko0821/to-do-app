aab53b2a46425d503a130dd3401f17cd2752abd0
Koji Moriyama <hocchiki.hasami0821@gmail.com>
2025-11-10 17:37:43 +0900

    feat: creating TodoItem components and styling it

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TodoItem from "./components/TodoItem"
+import { dummyData } from "./data/todos"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <h1>YOUR TODOS</h1>
+      <div className="max-w-lg mx-auto">
+        <div className="space-y-2">
+          {dummyData.map(data => (
+            <p key={data.id} className="text-lg">
+              <TodoItem todo={data}/>
+            </p>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+import type { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id:1,
+        title: 'Like this video',
+        completed: false,
+    },
+    {
+        id:2,
+        title: 'Subscribe to Codingg in Flow',
+        completed: false,
+    },
+    {
+        id:3,
+        title: 'Like this videoLeave a nice commnet',
+        completed: false,
+    },
+
+]
